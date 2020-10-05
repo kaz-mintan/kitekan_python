6302c7144fdb04deb1cac22eeba860ea75f42c59
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-10-05 21:27:50 +0900

    add virtually generated data

--- weight_est/main.py
+++ weight_est/main.py
@@ -2,19 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def fit(phi_y,Y):
-  print(Y[:,0])
+from functions import *
+from virtual_sub import out_virtual_data
+
+def fit(phi_y,Y,t):
   if Y.ndim==1:
     PHI_X = np.array([phi_y(x) for x in Y])
   else:
     PHI_X = np.array([phi_y(Y[:,i]) for i in range(Y.shape[1])])
 
-  print('PHI_X',PHI_X.shape)
   w_x = np.linalg.solve(np.dot(PHI_X.T, PHI_X), np.dot(PHI_X.T, t))
-  print('w',w_x)
 
   ylist = [np.dot(w_x, phi_y(Y[:,i])) for i in range(Y.shape[1])]
-  print('yist',ylist)
 
   plt.plot(ylist,label="Y[0]")
   if Y.ndim==1:
@@ -25,20 +24,15 @@ def fit(phi_y,Y):
   plt.legend()
   plt.show()
 
+  return w_x
 
-factor = np.array([[0.01, 0.02, 0.1, 0.2, 0.3, 0.31, 0.32, 0.34, 0.35, 0.36],[0.02, 0.12, 0.19, 0.27, 0.42, 0.51, 0.64, 0.84, 0.88, 0.99],[0.4, 0.92, 0.91, 0.7, 0.4, 0.31, 0.5, 0.4, 0.8, 0.2]])
-Y = np.array([0.02, 0.12, 0.19, 0.27, 0.42, 0.51, 0.64, 0.84, 0.88, 0.99])
-t = np.array([0.05, 0.87, 0.94, 0.92, 0.54, -0.11, -0.78, -0.79, -0.89, -0.04])
+if __name__ == '__main__':
+  factor = np.array([[0.01, 0.02, 0.1, 0.2, 0.3, 0.31, 0.32, 0.34, 0.35, 0.36],[0.02, 0.12, 0.19, 0.27, 0.42, 0.51, 0.64, 0.84, 0.88, 0.99],[0.4, 0.92, 0.91, 0.7, 0.4, 0.31, 0.5, 0.4, 0.8, 0.2]])
+  Y = np.array([0.02, 0.12, 0.19, 0.27, 0.42, 0.51, 0.64, 0.84, 0.88, 0.99])
 
-plt.plot(factor[0],t,'*')
-plt.plot(factor[1],t,'+')
-plt.show()
-
-def phi_x(x):
-    return [1/(1+np.exp(x[0])),x[1]**3,np.exp(x[2]+1)]
-
-def phi_y(x):
-    return [x**3,np.exp(x)]
+  #t = np.array([0.05, 0.87, 0.94, 0.92, 0.54, -0.11, -0.78, -0.79, -0.89, -0.04])
+  weight = np.array([0.3,1,0.5])
+  t = out_virtual_data(phi_x,weight)
 
 #fit(phi_y,Y)
-fit(phi_x,factor)
+  print(fit(phi_x,factor,t))
