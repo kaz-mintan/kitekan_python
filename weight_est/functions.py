7b5be3574825265565682dd0e440257439e8a4d2
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-10-20 02:01:17 +0900

    separated function definition and added new function

--- weight_est/functions.py
+++ weight_est/functions.py
@@ -1,7 +1,17 @@
 import numpy as np
 
+def func_ex(x):
+    return 1.0/(1.0+np.exp(x))
+
 def phi_x(x):
-    return [1/(1+np.exp(x[0])),x[1]**3,np.exp(x[2]+1)]
+    return [func_ex(x[0]),x[1]**3,np.exp(x[2]+1)]
+
+def phi_ans(x):
+    return [func_ex(x[0]),x[1]**2,np.exp(x[2])]
+
+
+
+
 
 def phi(x):
     return [x**3,np.exp(x)]
