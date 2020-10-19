1857beb75c925972e77b294d719a94202e59edc0
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2020-10-20 02:02:55 +0900

    changed prog to make answer data, and make train/test data by extracting some from the answer data, and this prog checks output

--- weight_est/main.py
+++ weight_est/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from functions import *
-from virtual_sub import out_virtual_data
+from virtual_sub import out_ydata,out_virtual_data
 
 def fit(phi_y,Y,t):
   if Y.ndim==1:
@@ -13,26 +13,45 @@ def fit(phi_y,Y,t):
 
   w_x = np.linalg.solve(np.dot(PHI_X.T, PHI_X), np.dot(PHI_X.T, t))
 
-  ylist = [np.dot(w_x, phi_y(Y[:,i])) for i in range(Y.shape[1])]
-
-  plt.plot(ylist,label="Y[0]")
-  if Y.ndim==1:
-    plt.plot(Y, t, 'o',label='t')
-  else:
-    #for d in range(Y.ndim):
-    plt.plot(t, 'x',label='t')
-  plt.legend()
-  plt.show()
-
   return w_x
 
 if __name__ == '__main__':
-  factor = np.array([[0.01, 0.02, 0.1, 0.2, 0.3, 0.31, 0.32, 0.34, 0.35, 0.36],[0.02, 0.12, 0.19, 0.27, 0.42, 0.51, 0.64, 0.84, 0.88, 0.99],[0.4, 0.92, 0.91, 0.7, 0.4, 0.31, 0.5, 0.4, 0.8, 0.2]])
-  Y = np.array([0.02, 0.12, 0.19, 0.27, 0.42, 0.51, 0.64, 0.84, 0.88, 0.99])
+  for i in range(50):
+    dim = 3 
+    ans_weight = np.array([0.3,1,0.5])
+    ans_x = np.random.rand(dim,50)
 
-  #t = np.array([0.05, 0.87, 0.94, 0.92, 0.54, -0.11, -0.78, -0.79, -0.89, -0.04])
-  weight = np.array([0.3,1,0.5])
-  t = out_virtual_data(phi_x,weight)
+    train_time_length = 3
+    test_time_length = 10
 
-#fit(phi_y,Y)
-  print(fit(phi_x,factor,t))
+    train_id = np.sort(np.random.randint(0,49,(train_time_length,)))
+
+    train_factor = ans_x[:,train_id]
+    train_face = out_virtual_data(phi_ans,ans_weight,train_factor)
+
+    trained_w = fit(phi_x,train_factor,train_face)
+
+    test_id = np.sort(np.random.randint(0,49,(test_time_length,)))
+    test_factor = ans_x[:,test_id]
+
+    estimated_ylist = out_ydata(phi_x,trained_w,test_factor)
+
+    plt.scatter(test_id,estimated_ylist,label="estimated_y")
+    plt.scatter(train_id,train_face,label="trained_y")
+    plt.plot(range(50),out_ydata(phi_ans,ans_weight,ans_x),label="answer")
+
+    print('tra x,tes x',train_factor,test_factor)
+    print('weight compare',ans_weight,trained_w)
+    tra=np.array([out_ydata(phi_ans,trained_w,test_factor)])
+    ans=np.array([out_ydata(phi_x,ans_weight,test_factor)])
+    print('diff sum',np.sum(ans-tra))
+
+    plt.legend()
+    plt.show()
+
+  '''
+  ff=train_factor
+  noize = np.random.rand(*ff.shape)
+  print(noize)
+  test_factor = ff#+0.05*noize
+  '''
